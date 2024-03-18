fc57ee8539cf2bc5db56efdf681d5e488f288dd9
tehnickge <71207932+tehnickge@users.noreply.github.com>
2024-03-19 01:16:08 +0300

    fix styles

--- post/src/app/layout.tsx
+++ post/src/app/layout.tsx
@@ -19,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header></Header>
+      <Header></Header>
+        <main className="main-container">
         {children}
+        </main>
         <Footer></Footer>
       </body>
     </html>
--- post/src/app/page.tsx
+++ post/src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
 import { Typography } from "@mui/material";
+import Link from "next/link";
+
 
 const Home = () => {
   return (
     <main className={styles.main}>
-      <Typography variant="h5">SEX</Typography>
+      <Typography variant="h1">Welcome to task!</Typography>
+      <Link href="/posts"><Typography variant="h2">visit page {`"Posts"`}</Typography></Link>
     </main>
   );
 }
